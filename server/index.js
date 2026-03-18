5593d8d1f121c445333afb9757661605e05c5767
Aryan Singh <642singharyan@gmail.com>
2026-03-18 14:09:15 +0530

    Added client and server inside Eventora

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectToDB = require('./DB/db')
+
+dotenv.config();
+
+const authRoutes = require('./routes/auth');
+const eventRoutes = require('./routes/events');
+const bookingRoutes = require('./routes/bookings');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+connectToDB();
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/events', eventRoutes);
+app.use('/api/bookings', bookingRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
