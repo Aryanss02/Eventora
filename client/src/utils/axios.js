8899b606f7f238739b3b98fa8590222cda003f30
Aryanss02 <642singharyan@gmail.com>
2026-03-20 00:37:55 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://eventora-backend-g35z.onrender.com/api',
+  baseURL: 'https://eventora-n5jf.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
